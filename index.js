0c4a6adfd347aed332ec9912c1aa6792557db66a
xodnd0507 <24457545yong@gmail.com>
2024-07-30 18:32:30 +0900

    feat : Create login form

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 document.addEventListener('DOMContentLoaded', function(){
     const joinBtn = document.querySelector('.join_btn');
+    const loginBtn = document.querySelector('.login_btn');
     const joinModal = document.querySelector('.join_modal');
+    const loginModal = document.querySelector('.login_modal');
     const joinReset = document.querySelector('.join_reset');
+    const loginReset = document.querySelector('.login_reset');
 
     function modal(a,b,c){
         a.addEventListener('click', function(){
@@ -10,5 +13,9 @@ document.addEventListener('DOMContentLoaded', function(){
     }
 
     modal(joinBtn, joinModal, 'block');
+    modal(joinBtn, loginModal, 'none');
     modal(joinReset, joinModal, 'none');
+    modal(loginBtn, loginModal, 'block');
+    modal(loginBtn, joinModal, 'none');
+    modal(loginReset, loginModal, 'none');
 })
--- index.php
+++ index.php
@@ -19,6 +19,20 @@
         <button type="reset" class="join_reset">취소</button>
     </div>
 
+    <div class="login_modal">
+        <h2>로그인</h2>
+        <input type="text" name="userid" id="userid" placeholder="아이디"> <br>
+        <input type="password" name="userpw" id="userpw" placeholder="비밀번호"> <br>
+        <select name="member" id="member">
+            <option value='회원구분' selected disabled>회원구분</option>
+            <option value="관리자">관리자</option>
+            <option value="담당자">담당자</option>
+            <option value="일반회원">일반회원</option>
+        </select> <br>
+        <button type="submit">가입하기</button>
+        <button type="reset" class="login_reset">취소</button>
+    </div>
+
     <script src="index.js"></script>
 </body>
 </html>
