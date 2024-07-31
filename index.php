a2580e7f623935c7be1b485e035fef345aa2d813
xodnd0507 <24457545yong@gmail.com>
2024-07-31 16:34:25 +0900

    test : Create booth schedule

--- index.php
+++ index.php
@@ -14,6 +14,9 @@
 
         <?php if (isset($_GET['id']) && $_GET['id'] == 'admin'){ ?>
             <!-- 관리자 -->
+            <h3>관리자 페이지</h3>
+            <h5>부스 일정 생성 영역</h5>
+            <button id="openModalBtn">부스 일정 생성</button>
         <?php } elseif (isset($_GET['id']) && in_array($_GET['id'], ['man1', 'man2', 'man3'])){ ?>
             <!-- 담당자 -->
         <?php } elseif (isset($_GET['id'])) { ?>
@@ -24,9 +27,72 @@
         <?php } else { ?>
             <!-- 로그인X -->
         <?php } ?>
+
+        <div id="scheduleModal" class="modal">
+    <div class="modal-content">
+        <span class="close">&times;</span>
+        <h2>부스 일정 생성</h2>
+            <form id="scheduleForm">
+                <label for="operationDate">운영일자:</label>
+                <input type="date" id="operationDate" name="operationDate" required><br>
+
+                <label for="startTime">운영 시작 시간:</label>
+                <input type="time" id="startTime" name="startTime" min="09:00" max="19:00" required><br>
+
+                <label for="endTime">운영 종료 시간:</label>
+                <input type="time" id="endTime" name="endTime" min="09:00" max="19:00" required><br>
+
+                <label for="maxParticipants">최대 인원:</label>
+                <input type="number" id="maxParticipants" name="maxParticipants" min="1" required><br>
+
+                <button type="submit">일정 생성</button>
+            </form>
+        </div>
+    </div>
     
     <script src="http://code.jquery.com/jquery-latest.min.js"></script>
     <script src="index.js"></script>
     <script src="check_id.js"></script>
+    <script>
+        // scripts.js
+$(document).ready(function() {
+    var modal = $('#scheduleModal');
+    var openModalBtn = $('#openModalBtn');
+    var closeModalBtn = $('.close');
+  
+    openModalBtn.click(function() {
+      modal.show();
+    });
+  
+    closeModalBtn.click(function() {
+      modal.hide();
+    });
+  
+    $(window).click(function(event) {
+      if ($(event.target).is(modal)) {
+        modal.hide();
+      }
+    });
+  
+    $('#scheduleForm').submit(function(event) {
+      event.preventDefault();
+  
+      var formData = $(this).serialize();
+  
+      $.ajax({
+        type: 'POST',
+        url: 'create_schedule.php',
+        data: formData,
+        success: function(response) {
+          alert(response);
+          modal.hide();
+        },
+        error: function() {
+          alert('일정 생성 중 오류가 발생했습니다.');
+        }
+      });
+    });
+  });
+    </script>
 </body>
 </html>
