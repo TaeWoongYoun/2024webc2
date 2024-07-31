daeeb1972a63d22221238159e451834a6e41eaed
xodnd0507 <24457545yong@gmail.com>
2024-07-31 15:34:02 +0900

    test : Test user

--- index.php
+++ index.php
@@ -12,7 +12,18 @@
     <?php require('./login&join/join_modal.php')?>
     <?php require('./login&join/login_modal.php')?>
 
-    
+        <?php if (isset($_GET['id']) && $_GET['id'] == 'admin'){ ?>
+            <!-- 관리자 -->
+        <?php } elseif (isset($_GET['id']) && in_array($_GET['id'], ['man1', 'man2', 'man3'])){ ?>
+            <!-- 담당자 -->
+        <?php } elseif (isset($_GET['id'])) { ?>
+            <!-- 일반회원 -->
+            <h3>체험예약 페이지</h3>
+            <input type="text" placeholder="search"> <button>검색</button> <br>
+            <button>전체 일정 보기</button>
+        <?php } else { ?>
+            <!-- 로그인X -->
+        <?php } ?>
     
     <script src="http://code.jquery.com/jquery-latest.min.js"></script>
     <script src="index.js"></script>
